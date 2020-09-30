81e938df341f03761585da27495239cdd6454f07
Austin Lam <lamaustin68@gmail.com>
2020-09-29 19:29:39 -0700

    Initial Commit

---
+++ searchBarTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  searchBarTest2
+//
+//  Created by Austin Lam on 9/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
